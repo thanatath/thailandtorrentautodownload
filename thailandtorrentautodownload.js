57188158ba7efd6d43b28a4cd815711701da8851
Shi_ba'San <thanatuth.euro@gmail.com>
2019-02-10 17:14:08 +0700

    Create thailandtorrentautodownload.js

---
+++ thailandtorrentautodownload.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+var str = window.location.search;
+var strid = str.substring(6,11);
+if (strid =="sha1="){
+document.querySelector("i.fa.fa-download").click();
+}
+alert(strid);
