ceed4bc1df1f4a018d692758dc31e8421542b728
Shi_ba'San <thanatuth.euro@gmail.com>
2019-05-16 23:56:35 +0700

    Fix Web Detect Not Working

--- thailandtorrentautodownload.js
+++ thailandtorrentautodownload.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *.tt-torrent.com
+// @match        *.tt-torrent.com/*
 // @grant        none
 // ==/UserScript==
 
